184caba17a37c41b72cbb72b5dbd18658837e243
JC Chia <jingcheng19986630@outlook.com>
2021-08-15 23:37:58 +0800

    AEB feature completed

---
+++ include/jchia029_lab2/safety.h
@@ -0,0 +1,48 @@
+#ifndef SAFETY_H
+#define SAFETY_H
+
+#include <ros/ros.h>
+#include <cmath>
+#include <algorithm>
+
+#include <std_msgs/Bool.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
+
+#define PI 3.14159
+
+// The class that handles emergency braking
+class Safety {
+public:
+    // Constructor
+    Safety();
+
+    // Initialize publishers & subscribers API
+    void init_pub_sub();
+
+private:
+    ros::NodeHandle nh;
+
+    // Variables
+    double speed;
+    std_msgs::Bool brake_bool_msg;
+    ackermann_msgs::AckermannDriveStamped brake_msg;
+
+    // Declare Subscribers
+    ros::Subscriber scan_sub;
+    ros::Subscriber odom_sub;
+    
+    // Declare Publishers
+    ros::Publisher brake_bool_pub;
+    ros::Publisher brake_pub;
+
+    // Callback functions
+    void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg);
+    void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg);
+
+
+
+};
+
+#endif
---
+++ src/safety_node.cpp
@@ -0,0 +1,41 @@
+#include "jchia029_lab2/safety.h"
+
+Safety::Safety() : nh(), speed(0.0){ init_pub_sub();}
+
+void Safety::init_pub_sub(){
+    scan_sub = nh.subscribe("/scan", 1000, &Safety::scan_callback, this);
+    odom_sub = nh.subscribe("/odom", 1000, &Safety::odom_callback, this);
+    brake_bool_pub = nh.advertise<std_msgs::Bool>("/brake_bool", 1000);
+    brake_pub = nh.advertise<ackermann_msgs::AckermannDriveStamped>("/brake", 1000);
+}
+
+void Safety::odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
+    // Retrieve current speed for TTC caluclation
+    speed = odom_msg->twist.twist.linear.x;
+}
+
+void Safety::scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
+    
+    for (int i = 0; i < (scan_msg->ranges.size())/2; i++){  //Only take front ranging data
+        if(!std::isinf(scan_msg->ranges[i]) && !std::isnan(scan_msg->ranges[i])){
+            // Calculate TTC
+            double TTC = scan_msg->ranges[i] / std::max(0.0, (speed * cos( i * scan_msg->angle_increment - PI/2)));
+            
+            //publish brake messages when TTC less than minimum required TTC
+            if (TTC < 0.25){
+                // ROS_INFO("TTC%d: %f", i, TTC);  //Used for tuning min TTC
+                brake_bool_msg.data = true;
+                brake_msg.drive.speed = 0.0;
+                brake_bool_pub.publish(brake_bool_msg);
+                brake_pub.publish(brake_msg);
+            }
+        }
+    }
+}
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "safety_node");
+    Safety sn;
+    ros::spin();
+    return 0;
+}
